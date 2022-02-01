de3fff2116bf82bd024d57d6107e1f0315ab4597
user <ulanskii03@gmail.com>
2022-02-01 20:26:43 +0600

    test

---
+++ src/com/company/Vertolet.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.SortedMap;
+
+public class Vertolet {
+    String strana;
+    int god;
+    String model;
+    int v;
+    String cvet;
+    int dver;
+    String vint;
+    int baasy;
+
+    static void uchat(){
+        System.out.println("uchup baratat");
+    }static void atat(){
+        System.out.println("atyp jatat");
+    } static void konot() {
+        System.out.println("jerge konup jatat");
+    }
+
+
+}
