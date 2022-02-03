cd9cf8b85abb64a952e34d9d8f91035abd9e5c53
user <ulanskii03@gmail.com>
2022-02-03 11:52:46 +0600

    praktikaNou

---
+++ src/com/company/Nout.java
@@ -0,0 +1,52 @@
+package com.company;
+
+public class Nout {
+
+    private String name;
+    private int memory;
+    private String colour;
+    private String cpu;
+
+    static void vkl() {
+        System.out.println("nout vkluchaetsya");
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMemory() {
+        return memory;
+    }
+
+    public void setMemory(int memory) {
+        this.memory = memory;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public String getCpu() {
+        return cpu;
+    }
+
+    public void setCpu(String cpu) {
+        this.cpu = cpu;
+    }
+
+    static void otkl() {
+        System.out.println("nout vyrubaetsya");
+
+
+    }
+}
