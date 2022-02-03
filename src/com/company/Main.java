fc98b32bd1d8108da0e9786c9bd4f220488a7271
user <ulanskii03@gmail.com>
2022-02-03 11:43:55 +0600

    praktikaNout

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,28 @@
 package com.company;
 
 import java.util.Scanner;
+import java.util.SortedMap;
 
 public class Main {
 
+
+
     public static void main(String[] args) {
-        TriUgolnik tri = new TriUgolnik();
-        tri.area(3, 4, 5);
+
+        Nout nout = new Nout();
+        nout.setName("lenovo");
+        nout.setColour("krasnyi");
+        nout.setMemory(512);
+        nout.setCpu("windows");
+        System.out.println(nout.getName()+" "+nout.getCpu()+" "+nout.getColour()+" "+nout.getMemory() +" гб");
+
+        Nout.vkl();
+        Nout.otkl();
+
+
+
+
+
     }
 }
 
